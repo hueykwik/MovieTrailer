655cd04c7ea272911da24a37a0b72a721f600da2
Huey Kwik <huey.kwik@gmail.com>
2017-04-12 21:27:23 -0700

    Add example movies.

---
+++ entertainment_center.py
@@ -0,0 +1,15 @@
+import media
+
+shawshank = media.Movie('The Shawshank Redemption',
+                        'https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_UX182_CR0,0,182,268_AL_.jpg',
+                        'https://www.youtube.com/watch?v=6hB3S9bIaco')
+
+forrest = media.Movie('Forrest Gump',
+                      'https://images-na.ssl-images-amazon.com/images/M/MV5BYThjM2MwZGMtMzg3Ny00NGRkLWE4M2EtYTBiNWMzOTY0YTI4XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_UY268_CR10,0,182,268_AL_.jpg',
+                      'https://www.youtube.com/watch?v=uPIEn0M8su0')
+
+her = media.Movie('Her',
+                  'https://images-na.ssl-images-amazon.com/images/M/MV5BMjA1Nzk0OTM2OF5BMl5BanBnXkFtZTgwNjU2NjEwMDE@._V1_UX182_CR0,0,182,268_AL_.jpg',
+                  'https://www.youtube.com/watch?v=WzV6mXIOVl4')
+
+movies = [shawshank, forrest, her]
--- media.py
+++ media.py
@@ -1,7 +1,14 @@
 class Movie:
+    """A class that represents a single movie.
+
+    Attributes:
+        title: A string representing the movie tile.
+        poster_image_url: A string URL for the movie's poster image.
+        trailer_youtube_url: A string URL for the movie's trailer on YouTube.
+        eg
+    """
+
     def __init__(self, title, poster_image_url, trailer_youtube_url):
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-
-
