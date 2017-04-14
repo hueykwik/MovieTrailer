85fdd65c2ff38835fa2408119e9bfc8db10749fb
Huey Kwik <huey.kwik@gmail.com>
2017-04-14 01:17:19 -0400

    Fix style.

--- media.py
+++ media.py
@@ -1,4 +1,4 @@
-class Movie:
+class Movie(object):
     """A class that represents a single movie.
 
     Attributes:
@@ -9,6 +9,16 @@ class Movie:
     """
 
     def __init__(self, title, poster_image_url, trailer_youtube_url):
+        """Creates a movie.
+
+        Args:
+            title: the movie title
+            poster_image_url: url for the movie's image
+            trailer_youtube_url: url for movie's YouTube trailer
+
+        Returns:
+            A movie with the attributes specified in the arguments.
+        """
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
