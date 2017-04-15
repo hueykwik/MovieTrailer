6cb65fd2c77b1ffab37652319f5770730a99cb83
Huey Kwik <huey.kwik@gmail.com>
2017-04-15 16:12:46 -0400

    Add comment describing movie creation.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,5 +1,6 @@
 import media
 
+# Create movies for Shawshank, Forrest Gump, and Her.
 shawshank = media.Movie('The Shawshank Redemption',
                         'https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_UX182_CR0,0,182,268_AL_.jpg',  # noqa
                         'https://www.youtube.com/watch?v=6hB3S9bIaco')
